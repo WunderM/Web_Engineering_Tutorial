244d6746fd6c48c3a6ae0d4a730000f2dd104faa
WunderM <Maurice.Wunder@t-online.de>
2023-05-29 09:49:01 +0200

    [Named Slots] Durchgeschaut

--- VUE/js/components/AssignmentList.js
+++ VUE/js/components/AssignmentList.js
@@ -1,15 +1,20 @@
 import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
+import Panel from "./Panel.js";
 
 export default {
-    components: { Assignment, AssignmentTags },
+    components: { Assignment, AssignmentTags, Panel },
 
     template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
+        <Panel v-show="assignments.length" class="w-70">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
             
             <assignment-tags
                 v-model:currentTag="currentTag"
@@ -23,17 +28,20 @@ export default {
                     :assignment="assignment"
                 ></assignment>
             </ul>
-        </section> 
+            
+            <slot></slot>
+        </Panel> 
     `,
 
     props: {
         assignments: Array,
-        title: String
+        title: String,
+        canToggle: { type: Boolean, default: false }
     },
 
     data() {
         return {
-            currentTag: 'all'
+            currentTag: 'all',
         };
     },
 
